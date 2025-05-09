3b136401fe838504e19debccf1db3375d022550e
Will <whgriffiths1@sheffield.ac.uk>
2025-05-09 12:42:00 +0100

    seperated branch from bank

--- Branch.py
+++ Branch.py
@@ -3,9 +3,10 @@ from Staff import Staff
 
 class Branch:
 
-    def __init__(self, location):
+    def __init__(self, location, opening_time):
         self._location = location
         self._staff = []
+        self._branch_opening_times = opening_time
 
     def get_location(self):
         return self._location
@@ -15,3 +16,12 @@ class Branch:
 
     def get_staff(self):
         return self._staff
+    
+    def add_staff_member(self, staff: Staff):
+        self._staff.append(staff)
+
+    def get_opening_times(self):
+        return self._branch_opening_times
+
+    def change_opening_time(self, time: str):
+        self._branch_opening_times = time
